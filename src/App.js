ffac9aec09b1872e0307c187af7f3ab41df0bd2b
kiboma <omayioben@gmail.com>
2023-12-18 18:08:26 +0300

    Add to local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
@@ -6,12 +6,11 @@ import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
-  const [customers, setCustomers] = useState ([
-    {id: 1234, name: 'James Atoti', age: 26, employed: false},
-    {id : 1245, name: 'Abraham Lincolin', age: 44, employed: true},
-    {id: 6985, name: 'Jane Adongo', age:31, employed: true},
-    {id: 6987, name: 'Fatma Mburu', age:19, employed: false},
-]);
+  const [customers, setCustomers] = useState (JSON.parse(localStorage.getItem("customers")) || []);
+
+  useEffect(() =>{
+    localStorage.setItem("customers", JSON.stringify(customers));
+  })
 
 
   return (
