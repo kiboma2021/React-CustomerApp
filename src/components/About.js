907cd493579b46d452d309ed5a21372d59ae827a
kiboma <omayioben@gmail.com>
2023-12-20 09:05:54 +0300

    Navigate to home page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
+    const navigate = useNavigate();
   return (
-    <div className='route-pg'>
+    <div className='route-pg' style={{flexDirection:'column'}}>
 
         <h1>Our about us page</h1>
+        <button onClick={()=>navigate('/')} style={{backgroundColor:'blue',color:'white', padding:'1rem',border:'none',borderRadius:'2rem',margin:'1rem'}} >Go back to Home Page</button>
       
     </div>
   )
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Contacts = () => {
+    const navigate = useNavigate()
   return (
     <div className='route-pg' style={{flexDirection:'column'}}>
         <h1>This is contact page</h1>
-        <button style={{backgroundColor:'blue',color:'white', padding:'1rem',border:'none',borderRadius:'2rem',margin:'1rem'}} >Go back to Home Page</button>
+        <button onClick={()=>navigate('/')} style={{backgroundColor:'blue',color:'white', padding:'1rem',border:'none',borderRadius:'2rem',margin:'1rem'}} >Go back to Home Page</button>
       
     </div>
   )
